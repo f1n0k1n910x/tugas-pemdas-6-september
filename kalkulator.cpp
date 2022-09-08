530ae20194b99f500fe6f337158994776373eb31
f1n0k1n910x <112488936+f1n0k1n910x@users.noreply.github.com>
2022-09-08 09:01:54 +0700

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int bil1,bil2, pil;
+	float hasil;
+	string operasi;
+	
+	cout<<"PILIH OPERATOR ARITMATIKA"<<endl;
+	cout<<"1. Penjumlahan"<<endl;
+	cout<<"2. Pengurangan"<<endl;
+	cout<<"3. Perkalian"<<endl;
+	cout<<"4. Pembagian"<<endl;
+	cout<<"5. Modulus"<<endl;
+	cout<<endl;
+	
+	cout<<"Masukkan Pilihan : ";
+	cin>>pil;
+	cout<<"Masukkan Bilangan Pertama : ";
+	cin>>bil1;
+	cout<<"Masukkan Bilangan Kedua : ";
+	cin>>bil2;
+	
+	switch(pil){
+			case 1 : hasil=bil1+bil2;
+			operasi='+';
+			break;
+		case 2 : hasil=bil1+bil2;
+			operasi='-';
+			break;
+		case 3 : hasil=bil1+bil2;
+			operasi='*';
+			break;
+		case 4 : hasil=bil1+bil2;
+			operasi='/';
+			break;
+		case 5 : hasil=bil1+bil2;
+				operasi='%';
+			break;
+		default : 
+			cout<<"Salah Masukan Operator"<<endl;
+	}
+	
+	cout<<"-----------------------------"<<endl;
+	cout<<"    "<<bil1<<operasi<<bil2<<"="<<hasil<<endl;
+	cout<<"-----------------------------"<<endl;
+	return 0;
+}
